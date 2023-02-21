ed4c6d107b9b9dc8c1ac5fb28d32271cf1d1abff
nazim_gitlab <skabdulnazim428@gmail.com>
2023-02-21 11:51:22 +0530

    authorization

---
+++ src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'jwt-secret',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
